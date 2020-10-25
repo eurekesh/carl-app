752999aab991edd1c7ec8a222fa5be67effa25b8
eurekesh <asdu8905@colorado.edu>
2020-10-25 13:39:57 -0600

    ejs is DONE!!

--- app.js
+++ app.js
@@ -10,15 +10,24 @@ serv.listen(port,()=> {
 });
 
 //dynamic routes go here, look into socket.io "rooms"
-app.set('views', __dirname+'/public/views');
+app.set('views', [__dirname+'/public/views/pages', __dirname+'/public/views/about-me-pages']);
+//app.set('img', __dirname+'/public/views/about-me-pages/img');
+
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname+ "public/"))
 
 app.get("/", function (req, res) {
   //res.sendFile(__dirname +'/public/about-me-pages/about-us.ejs')
-  res.render('pages/instructions.ejs')
+  res.render('instructions')
 });
 
+app.get("/res/:folder/:filen", function (req, res) { // yikes this took a long time
+                                             //res.sendFile(__dirname +'/public/about-me-pages/about-us.ejs')
+  let curr_path = path.parse(req.originalUrl);
+  console.log('req.originalURl for res is ' + req.originalUrl)
+  res.sendFile(__dirname + '/public/res/' + req.params.folder + '/' + req.params.filen);
+
+});
 app.get("/:req_page/", function (req, res) { // yikes this took a long time
   //res.sendFile(__dirname +'/public/about-me-pages/about-us.ejs')
   let curr_path = path.parse(req.originalUrl);
@@ -29,7 +38,7 @@ app.get("/:req_page/", function (req, res) { // yikes this took a long time
   }
   else{
     console.log('attempting to render '+req.params.req_page);
-    res.render('pages/' + req.originalUrl);
+    res.render(req.params.req_page);
   }
   console.log(req.params);
 
@@ -40,14 +49,17 @@ app.get("/about-me-pages/:req_page", function (req, res) { // yikes this took a 
   let curr_path = path.parse(req.originalUrl);
   console.log('req.originalURl is ' + req.originalUrl)
   console.log('req.path is ' + req.path)
+
   if(curr_path.ext !== '' && curr_path.ext !== '.ejs')
   {
-    res.sendFile(__dirname+'/public/views/about-me-pages'+req.originalUrl)
+    console.log('attempting to send file path ' + req.originalUrl)
+    res.sendFile(__dirname+'/public/views/about-me-pages/img/'+req.params.req_page)
   }
   else{
     console.log('attempting to render '+req.params.req_page);
-    res.render('about-me-pages/' + req.params.req_page);
+    res.render(req.params.req_page);
   }
+
   console.log(req.params);
 
 });

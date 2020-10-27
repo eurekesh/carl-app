85921f4bdf9487b525dabe6c5f20bc9a5def9ee4
root <root@DESKTOP-HAJM498.localdomain>
2020-10-27 00:22:38 -0600

    Still not working and julia is sad she wasted so much time

--- app.js
+++ app.js
@@ -72,3 +72,53 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('to_client',data);
   })
 });
+
+
+
+
+
+
+//Create Database Connection
+var pgp = require('pg-promise')();
+
+/**********************
+  Database Connection information
+  host: This defines the ip address of the server hosting our database.  We'll be using localhost and run our database on our local machine (i.e. can't be access via the Internet)
+  port: This defines what port we can expect to communicate to our database.  We'll use 5432 to talk with PostgreSQL
+  database: This is the name of our specific database.  From our previous lab, we created the football_db database, which holds our football data tables
+  user: This should be left as postgres, the default user account created when PostgreSQL was installed
+  password: This the password for accessing the database.  You'll need to set a password USING THE PSQL TERMINAL THIS IS NOT A PASSWORD FOR POSTGRES USER ACCOUNT IN LINUX!
+**********************/
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'carl',
+	user: 'postgres',
+	password: 'CARL',
+};
+
+var db = pgp(dbConfig);
+
+
+
+
+
+//get request to display all old canvases in order of date created 
+app.get('/past-drawings', function(req, res) {
+	var query = 'SELECT img_64bitcode FROM canvases ORDER BY date;';
+	db.any(query)
+        .then(function (rows) {
+            res.render('views/past-drawings',{
+				my_title: "Past Drawings",
+				data: rows
+			})
+
+        })
+        .catch(function (err) {
+            console.log('error', err);
+            res.render('views/past-drawings', {
+                my_title: 'Past Drawings',
+                data: ''
+            })
+        })
+});
